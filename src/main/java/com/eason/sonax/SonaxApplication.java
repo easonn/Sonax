084fe3271e2b619ff02ba81b9e716c1040ab5afd
eason <yinsheng92@hotmail.com>
2018-04-14 10:49:44 +0800

    add swagger2 ,redis

--- src/main/java/com/eason/sonax/SonaxApplication.java
+++ src/main/java/com/eason/sonax/SonaxApplication.java
@@ -2,19 +2,14 @@ package com.eason.sonax;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@RestController
+@EnableSwagger2
 public class SonaxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SonaxApplication.class, args);
 	}
 
-	@GetMapping("hi")
-	public  String hi() {
-		return "hi, I'm forezp";
-	}
 }
